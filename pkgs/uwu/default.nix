7ba0c211909d611a420101a076d0ee70b17c8e9b
VojtechStep <vojtechstepancik@outlook.com>
2021-08-09 00:01:04 +0200

    Add uwu

---
+++ pkgs/uwu/default.nix
@@ -0,0 +1,19 @@
+{ stdenv, fetchFromGitHub }:
+
+let
+  name = "uwu";
+  version = "0.1";
+in stdenv.mkDerivation {
+  inherit name version;
+  src = fetchFromGitHub {
+    owner = "98WuG";
+    repo = name;
+    rev = "v${version}";
+    sha256 = "1n9laj9976lcw96baavhz9zmsaidv2z6qmb13g3wdwm028bq3wzr";
+  };
+  buildPhase = "make";
+  installPhase = ''
+    mkdir -p $out/bin
+    install -Dm755 uwu $out/bin
+  '';
+}
